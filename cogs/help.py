9ecbe6a2d404db533cf0408c6d71445a569176a2
siwoo131 <tigersiwoo131@gmail.com>
2021-07-02 10:06:34 +0900

    진행중 ===> 준비중

--- cogs/help.py
+++ cogs/help.py
@@ -70,7 +70,7 @@ class Help(commands.Cog, name="도움"):
     @commands.command(name="진행중인작업", help="현재 진행중인 작업을 알려줍니다")
     async def process(self, ctx):
         embed = discord.Embed(color=0x00FF21, title="현재 진행중인 작업",
-                              description="현재 Kubot은 파이썬에서 node.js로의 이전을 진행중입니다.")
+                              description="현재 Kubot은 파이썬에서 node.js로의 이전을 준비중입니다.")
         embed.set_footer(text="bot made by. 미간#8269",
                          icon_url="https://cdn.discordapp.com/avatars/415135882006495242/cb4c1c1fce24f512e07f673989814572.webp?size=1024")
         await ctx.send(embed=embed)
