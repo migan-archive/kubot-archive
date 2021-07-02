f7cc2d06b9b094e08755326abd27f02944e8a7f9
siwoo131 <tigersiwoo131@gmail.com>
2021-07-02 21:32:20 +0900

    수정

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,10 @@
 module.exports = {
     name: "핑",
     run(client, message, args) {
-        const ping = Date.now() - message.createdTimestamp;
         const Discord = require('discord.js');
         const Embed = new Discord.MessageEmbed()
             .setTitle(":ping_pong:퐁!")
-            .setDescription(`\`${ping}\`ms`)
+            .setDescription(`\`${client.ws.ping}\`ms`)
             .setColor("00FF21")
             .setTimestamp(Date.now())
             .setFooter("Bot Made by. 미간#8269", "https://cdn.discordapp.com/avatars/415135882006495242/cb4c1c1fce24f512e07f673989814572.webp?size=1024")
