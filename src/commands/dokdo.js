d754a5c5aceaac95051f83f564422f0aed9d108b
siwoo131 <tigersiwoo131@gmail.com>
2021-07-12 08:26:54 +0900

    edit: Dokdo Command

--- src/commands/dokdo.js
+++ src/commands/dokdo.js
@@ -4,15 +4,16 @@ const Dokdo = require('dokdo');
 module.exports = {
     name: "dokdo",
     async run(client, message, args) {
-        if(message.author.id !== "415135882006495242") return message.channel.send(
-            new Discord.MessageEmbed()
-                .setColor("00FF21")
-                .setTitle("Dokdo 커맨드")
-                .setDescription("이 명령어는 개발자만 쓸쑤있어요!")
-                .setTimestamp(Date.now())
-                .setFooter(message.author.username, message.author.displayAvatarURL())
-        );
-        const DokdoHandler = new Dokdo(client, { aliases: ['dokdo', 'dok'], prefix: '--', owners: "415135882006495242" }); // 여기있는 prefix는 dokdo 커맨드 전용 접두사입니다./ owners 안애다가 오너의 ID를 넣어주세요!
+        const DokdoHandler = new Dokdo(client, {
+            aliases: ['dokdo', 'dok'], prefix: '--', owners: "415135882006495242", disableAttachmentExecution: true, noPerm: (message) => message.reply(
+                new Discord.MessageEmbed()
+                    .setColor("00FF21")
+                    .setTitle("Dokdo 커맨드")
+                    .setDescription("이 명령어는 개발자만 쓸쑤있어요!")
+                    .setTimestamp(Date.now())
+                    .setFooter(message.author.username, message.author.displayAvatarURL())
+            )
+        }); // 여기있는 prefix는 dokdo 커맨드 전용 접두사입니다./ owners 안애다가 오너의 ID를 넣어주세요!
         DokdoHandler.run(message);
     }
 }
