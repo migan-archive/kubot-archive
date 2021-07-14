ada8801056af2c3d3bfbc3949a567d4679cc1faf
siwoo131 <tigersiwoo131@gmail.com>
2021-07-14 19:58:07 +0900

    add: say.js

---
+++ src/commands/say.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "따라해",
+    run(client, message, args) {
+        if (args[0] === null) return message.reply("따라할 메세지를 적어 주세요!");
+        if (args[0] === "@everyone") return message.reply("해당 메세지엔 에브리원이 들어가 있어 따라할수 없습니다!");
+        message.channel.send(args[0]);
+    }
+}
