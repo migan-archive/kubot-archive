9bab658ceed8a4d1661d9a931a4b388a7ed2a73b
siwoo131 <tigersiwoo131@gmail.com>
2021-07-16 21:47:30 +0900

    EDIT

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: npm run r
+worker: npm run start
--- src/commands/say.js
+++ src/commands/say.js
@@ -1,8 +1,9 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: "따라해",
     run(client, message, args) {
         if (args[0] == null) return message.reply("따라할 메세지를 적어 주세요!");
-        if (args[0] === "@everyone") return message.reply("해당 메세지엔 에브리원이 들어가 있어 따라할수 없어요!");
-        message.channel.send(args[0]);
+        message.channel.send(args[0], { disableMentions: 'all' });
     }
 }
