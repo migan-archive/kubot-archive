af24b95236d9e8dc8d83170fa9dbb9df214476eb
siwoo131 <tigersiwoo131@gmail.com>
2021-07-19 10:45:16 +0900

    fix: DiscordApiError (no send DM)

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -8,18 +8,10 @@ module.exports = {
         let reason = args.slice(1).join(" ");
         if (!reason) reason = " 없음";
 
-        const banEmbed = new Discord.MessageEmbed()
-            .setColor("00FF21")
-            .setTitle("차단")
-            .setDescription(`당신은 ${message.guild.name}에서 차단되었습니다!\n이유: ${reason}`)
-            .setTimestamp(Date.now())
-            .setFooter(`관리자:${message.author.tag}`, message.author.displayAvatarURL());
-
         if (!args[0]) return message.reply("밴할 사용자를 지정해야 해요!");
         if(!mentionMember) return message.channel.send("이 사용자는 유효하지 않거나 더 이상 서버에 없습니다!");
         if(!mentionMember.bannable) return message.channel.send("이사용자는 밴을 할수없습니다!");
 
-        await mentionMember.send(banEmbed);
         await mentionMember.ban({
             reason: reason
         }).then(() => message.channel.send(`성공적으로 밴을 하였습니다! 밴한 사용자: ${mentionMember.user.tag}`));
--- src/commands/kick.js
+++ src/commands/kick.js
@@ -8,22 +8,9 @@ module.exports = {
         let reason = args.slice(1).join(" ");
         if (!reason) reason = " 없음";
 
-        const kickEmbed = new Discord.MessageEmbed()
-            .setColor("00FF21")
-            .setTitle("추방")
-            .setDescription(`당신은 ${message.guild.name}에서 추방되었습니다!\n이유: ${reason}`)
-            .setTimestamp(Date.now())
-            .setFooter(`관리자:${message.author.tag}`, message.author.displayAvatarURL());
-
         if (!args[0]) return message.reply("킥할 사용자를 지정해야 해요!");
         if(!mentionMember) return message.channel.send("이 사용자는 유효하지 않거나 더 이상 서버에 없습니다!");
         if(!mentionMember.kickable) return message.channel.send("이사용자는 킥을 할수없습니다!");
-
-        try {
-            await mentionMember.send(kickEmbed);
-        } catch (err) {
-
-        }
 
         try {
             await mentionMember.kick(reason);
