8fce2494dfc9492de5cd966790696806ce48d3c0
siwoo131 <tigersiwoo131@gmail.com>
2021-07-27 21:16:16 +0900

    edit: random_number.js

--- src/commands/random_number.js
+++ src/commands/random_number.js
@@ -1,7 +1,10 @@
 module.exports = {
-    name: "랜덤숫자",
-    run(client, message, args) {
-        const random = Math.floor(Math.random() * 10000000000001);
-        message.channel.send(random);
-    }
-}
+	name: '랜덤숫자',
+	run(client, message, args) {
+        if (!args[0]) return message.reply("랜덤으로 할 숫자를 알려주세요!");
+        if (isNaN(args[0])) return message.reply("숫자를 입력해 주세요!");
+        if (args[0] < 1) return message.reply("1이상으로 해주세요!");
+
+        message.reply(`${Math.floor(Math.random() * args[0])}`);
+	}
+};
