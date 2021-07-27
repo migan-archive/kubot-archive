ee79089998ac07d97522a048b45fcc5b54af54a2
siwoo131 <tigersiwoo131@gmail.com>
2021-07-27 20:05:54 +0900

    improvement: say.js

--- src/commands/say.js
+++ src/commands/say.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: "따라해",
     run(client, message, args) {
-        if (args[0] == null) return message.reply("따라할 메세지를 적어 주세요!");
-        message.channel.send(args[0], { disableMentions: 'all' });
+        let text = args.join(" ");
+        if (!text) return message.reply("따라할 메세지를 적어 주세요!");
+        message.channel.send(text, { disableMentions: 'all' });
     }
 }
