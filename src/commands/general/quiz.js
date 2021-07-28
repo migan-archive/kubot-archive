1c2b292313eb6d90b7da785c4175e444240feafa
siwoo131 <tigersiwoo131@gmail.com>
2021-07-28 16:09:55 +0900

    add: quiz

---
+++ src/commands/general/quiz.js
@@ -0,0 +1,25 @@
+const quiz = require('../../../quiz.json');
+
+module.exports = {
+    name: "퀴즈",
+    aliases: ["quiz"],
+    run(client, message, args) {
+        const item = quiz[Math.floor(Math.random() * quiz.length)];
+        const limit = 3;
+
+        const filter = (response) => {
+            return item.answer.some((answer) => answer === response.content);
+        };
+
+        message.channel.send(`${item.question} 제한시간: ${limit}초`)
+            .then(() => {
+                message.channel.awaitMessages(filter, { max: 3, time: limit * 1000 })
+                    .then((collected) => {
+                        message.channel.send(`${collected.first().author} 정답!`);
+                    })
+                    .catch((err) => {
+                        message.channel.send('제한시간 초과!');
+                    });
+            });
+    }
+}
