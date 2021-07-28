618cb0ebb31624f9e4d6227053972fd546133e8d
siwoo131 <tigersiwoo131@gmail.com>
2021-07-28 11:42:53 +0900

    edit

--- src/index.js
+++ src/index.js
@@ -12,23 +12,28 @@ const clientKRBots = new KoreanbotsClient({
 })
 const Dokdo = require('dokdo');
 const DokdoHandler = new Dokdo(
-    client,
-    {
-      aliases: ['dokdo', 'dok', "독도", "debug", "debugging", "Dok", "Dokdo"],
-      prefix: '--', /* 여기 있는 prefix는 독도에 prefix 입니다 */
-      noPerm: (message) => message.reply("어라? 당신은 개발자가 아닌데요?") /* 여기는 개발자가 아닐때 보내는 메세지 입니다. */
+  client,
+  {
+    aliases: ['dokdo', 'dok', "독도", "debug", "debugging", "Dok", "Dokdo"],
+    prefix: '--', /* 여기 있는 prefix는 독도에 prefix 입니다 */
+    noPerm: (message) => message.reply("어라? 당신은 개발자가 아닌데요?") /* 여기는 개발자가 아닐때 보내는 메세지 입니다. */
   }
 );
-const help = require('./commands/help.js');
+// const help = require('./commands/general/help.js');
 
 client.commands = new Discord.Collection()
 
 process.env.SHELL = '/bin/bash'; /* 이건 리눅스 bash 쉘입니다. */
 
 client.commands.load = dir => {
-  for (const file of fs.readdirSync(dir)) {
-    const cmd = require(`./commands/${file}`);
-    client.commands.set(cmd.name, cmd);
+  const commandFolders = fs.readdirSync(__dirname + '/commands');
+
+  for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(__dirname + `/commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+      const command = require(__dirname + `/commands/${folder}/${file}`);
+      client.commands.set(command.name, command);
+    }
   }
   console.log(client.commands.map(c => c.name).join(', ') + ' Load Success');
 }
@@ -69,14 +74,14 @@ client.on('message', msg => {
   if (msg.author.bot) return;
   DokdoHandler.run(msg);
   if (msg.content === `<@!${client.user.id}>`) {
-      const Embed = new Discord.MessageEmbed()
-          .setColor("00FF21")
-          .setTitle(`${client.user.username}이에요!`)
-          .setDescription(`저의 접두사는 \`${prefix}\`이에요!\n\`${prefix}${help.name}\`로 명령어를 확인해 주세요!`)
-          .setTimestamp(Date.now())
-          .setFooter(msg.author.tag, msg.author.displayAvatarURL());
+    const Embed = new Discord.MessageEmbed()
+      .setColor("00FF21")
+      .setTitle(`${client.user.username}이에요!`)
+      .setDescription(`저의 접두사는 \`${prefix}\`이에요!\n\`${prefix}도움말\`로 명령어를 확인해 주세요!`)
+      .setTimestamp(Date.now())
+      .setFooter(msg.author.tag, msg.author.displayAvatarURL());
     msg.reply(Embed);
-  }
+  };
 
   if (msg.content === "쿠봇아 안녕" || msg.content === "쿠봇아 안뇽" || msg.content === "쿠봇아 하이") {
     const list = ["안녕", "hi", "안녕하세요", "hello", "좋은아침이에요!"];
@@ -94,7 +99,7 @@ client.on('message', msg => {
 
   if (msg.content === "쿠봇아 뭐해") return msg.reply("저는 사람들이랑 대화중이에요!");
   if (msg.content === "쿠봇아 바보") return msg.reply("바보 아닌데요");
-  if (msg.content ==="쿠봇아 야") {
+  if (msg.content === "쿠봇아 야") {
     const list = ["뭐", "왜"];
     const random = Math.floor(Math.random() * 2);
     const hey = list[random];
@@ -113,7 +118,7 @@ client.on('message', msg => {
 
   let cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
 
-  if (!command) return;
+  if (!cmd) return;
 
   if (cmd) cmd.run(client, msg, args); /* 나중에 핑퐁 빌더로 대화 기능 수정할껍니다. */
 });
