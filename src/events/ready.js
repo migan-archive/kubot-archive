b42f81c2b6d8729b037a5e9c05c4861f1d2ba5fd
siwoo131 <tigersiwoo131@gmail.com>
2021-09-03 15:54:58 +0900

    fix: author name

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
         console.log(client.commands.map(c => c.name).join(', ') + ' Load Success');
         console.log(`Login: ${client.user.username}`);
         console.log("Licence: MIT");
-        console.log("Author: 미간#8269"); // 이건 수정 하시면 안됩니다.
+        console.log("Author: Migan#8269"); // 이건 수정 하시면 안됩니다.
         console.log(`prefix: ${client.prefix}`)
         console.log("======================================");
         client.user.setActivity(`${client.prefix}도움말`, { type: "PLAYING" });
