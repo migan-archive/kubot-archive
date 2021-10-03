7a7797e37c237b7f7938ba3113914afb202ffc5f
siwoo131 <tigersiwoo131@gmail.com>
2021-10-03 22:20:04 +0900

    fix: ping

--- src/commands/utils/ping.js
+++ src/commands/utils/ping.js
@@ -3,17 +3,34 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
   name: '핑',
   aliases: ['ping'],
-  run(client, message, args) {
-    const Embed = new MessageEmbed()
-      .setTitle(':ping_pong:퐁!')
-      .setDescription(
-        `웹소켓 핑: \`${client.ws.ping}\`ms\n메세지 핑: \`${
-          Date.now() - message.createdTimestamp
-        }\`ms`
-      )
-      .setColor(client.EmbedColor)
-      .setTimestamp(Date.now())
-      .setFooter(message.author.tag, message.author.displayAvatarURL())
-    message.channel.send({ embeds: [Embed] })
+  async run(client, message, args) {
+    const msg = await message.channel.send({
+      embeds: [
+        new MessageEmbed()
+          .setTitle(':ping_pong:퐁!')
+          .setDescription(`웹소켓 핑: 측정중\n메세지 핑: 측정중`)
+          .setColor(client.EmbedColor)
+          .setTimestamp(Date.now())
+          .setFooter(message.author.tag, message.author.displayAvatarURL()),
+      ],
+    })
+    setInterval(
+      () =>
+        msg.edit({
+          embeds: [
+            new MessageEmbed()
+              .setTitle(':ping_pong:퐁!')
+              .setDescription(
+                `웹소켓 핑: \`${client.ws.ping}ms\`\n메세지 핑: \`${
+                  msg.createdTimestamp - message.createdTimestamp
+                }ms\``
+              )
+              .setColor(client.EmbedColor)
+              .setTimestamp(Date.now())
+              .setFooter(message.author.tag, message.author.displayAvatarURL()),
+          ],
+        }),
+      1000
+    )
   },
 }
