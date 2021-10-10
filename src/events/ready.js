a8c5870cce9e48d2ee714f7bdf3cb8039ed23234
siwoo131 <tigersiwoo131@gmail.com>
2021-10-10 19:03:19 +0900

    refactor: replace PLAYING to WATCHING

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,11 +1,17 @@
+const { Client } = require('discord.js')
+
 module.exports = {
   name: 'ready',
   once: true,
+  /**
+   *
+   * @param {Client} client
+   */
   execute(client) {
     console.log(client.commands.map(c => c.name).join(', ') + ' Load Success')
     console.log(`Login: ${client.user.username}`)
     console.log('======================================')
-    client.user.setActivity(`${client.prefix}도움말`, { type: 'PLAYING' })
+    client.user.setActivity(`${client.prefix}도움말`, { type: 'WATCHING' })
     client.serverUpdate(client.guilds.cache.size)
     setInterval(() => client.serverUpdate(client.guilds.cache.size), 600000)
   },
